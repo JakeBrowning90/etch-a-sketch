6fd82f938c3991977c06871c55ba3608943ac25f
Jake Browning <jacobbrowning90@gmail.com>
2022-10-08 14:04:44 +0100

    Add event listeners to change class of squares on mousedown and mouseover for drawing

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+// Set default canvas size on page load
 let canvasSize = 16;
 const canvas = document.querySelector(".canvas");
+drawGrid(canvasSize);
+let isDrawing = false;
 
-for (i = 0; i < canvasSize; i++) {
-    const canvasRow = document.createElement("canvasRow");
-    canvasRow.classList.add("canvasRow");
-    for (j = 0; j < canvasSize; j++) {
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        canvasRow.appendChild(pixel);
+// "Refresh" button logic
+const refresh = document.getElementById("refresh");
+refresh.addEventListener("click", function (e) {
+    //Clear existing grid
+    clearGrid(canvas);
+    //Update grid with number input
+    canvasSize = document.getElementById("canvasSize").value;
+    drawGrid(canvasSize);
+});
+
+function clearGrid(canvas) {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
     }
-    canvas.appendChild(canvasRow);
+}
+
+function drawGrid(canvasSize) {
+    if (canvasSize > 1 && canvasSize < 101) {
+        for (i = 0; i < canvasSize; i++) {
+            const canvasRow = document.createElement("canvasRow");
+            canvasRow.classList.add("canvasRow");
+            for (j = 0; j < canvasSize; j++) {
+                const pixel = document.createElement("div");
+                pixel.classList.add("pixel");
+                canvasRow.appendChild(pixel);
+            }
+            canvas.appendChild(canvasRow);
+        }
+    }
+    else {
+        return;
+    }
 }
 
 canvas.addEventListener('mousedown', function (e) {
@@ -17,5 +43,23 @@ canvas.addEventListener('mousedown', function (e) {
         //console.log(e.target);
         e.target.classList.add("colored");
         //e.target.style.backgroundColor = 'black';
+        isDrawing = true;
     }
   });
+
+  canvas.addEventListener('mousemove', function (e) {
+    if (isDrawing) {
+        if(e.target.classList.contains("pixel")) {
+            //console.log(e.target);
+            e.target.classList.add("colored");
+            //e.target.style.backgroundColor = 'black';
+        }
+    }   
+  });
+
+  canvas.addEventListener('mouseup', function (e) {
+    if (isDrawing) {
+        isDrawing = false;
+    }   
+  });
+
