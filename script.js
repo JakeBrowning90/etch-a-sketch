fea2dba1df10900e8f7628aaeb8a8e70b1cd465e
Jake Browning <jacobbrowning90@gmail.com>
2022-10-09 17:14:01 +0100

    Add hex code input option to palette.

--- script.js
+++ script.js
@@ -14,6 +14,48 @@ refresh.addEventListener("click", function (e) {
     drawGrid(canvasSize);
 });
 
+
+
+canvas.addEventListener('mousedown', function (e) {
+    if(e.target.classList.contains("pixel")) {
+        drawOver(e.target);
+        colour = getColour();
+        if (colour == "Random") {
+            colourRandom(e.target);
+        }
+        else if (colour == "Custom"){
+            colourCustom(e.target);
+        }
+        else {
+            e.target.classList.add(`ink${colour}`);
+        }
+        isDrawing = true; 
+    }
+  });
+
+canvas.addEventListener('mousemove', function (e) {
+    if (isDrawing) {
+        if(e.target.classList.contains("pixel")) {
+            drawOver(e.target);
+            if (colour == "Random") {
+                colourRandom(e.target);
+            }
+            else if (colour == "Custom") {
+                colourCustom(e.target);
+            }
+            else {
+                e.target.classList.add(`ink${colour}`);
+            }
+        }
+    }   
+  });
+
+canvas.addEventListener('mouseup', function (e) {
+    if (isDrawing) {
+        isDrawing = false;
+    }   
+  });
+
 function clearGrid(canvas) {
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
@@ -38,96 +80,27 @@ function drawGrid(canvasSize) {
     }
 }
 
-canvas.addEventListener('mousedown', function (e) {
-    if(e.target.classList.contains("pixel")) {
-        let currentColour = e.target.classList[1];
-        e.target.classList.remove(currentColour);
-        e.target.style.removeProperty("background-color");
-        e.target.style.removeProperty("border-color");
+function colourRandom(target) {
+    let randomColour = Math.floor(Math.random()*16777215).toString(16);
+    target.style.backgroundColor = "#" + randomColour;
+    target.style.borderColor = "#" + randomColour;
+}
 
-        colour = getColour();
-        if (colour == "Random") {
-            let randomColour = Math.floor(Math.random()*16777215).toString(16);
-            e.target.style.backgroundColor = "#" + randomColour;
-            e.target.style.borderColor = "#" + randomColour;
-        }
-        else {
-            e.target.classList.add(`ink${colour}`);
-        }
-        isDrawing = true; 
-    }
-  });
+function colourCustom(target) {
+    let hexCode = document.getElementById("hexCode").value;
+    target.style.backgroundColor = "#" + hexCode;
+    target.style.borderColor = "#" + hexCode;
+}
 
-canvas.addEventListener('mousemove', function (e) {
-    if (isDrawing) {
-        if(e.target.classList.contains("pixel")) {
-            let currentColour = e.target.classList[1];
-            e.target.classList.remove(currentColour);
-            e.target.style.removeProperty("background-color");
-            e.target.style.removeProperty("border-color");
-
-            //getColour()
-            //e.target.classList.add(`ink${colour}`);
-            if (colour == "Random") {
-                let randomColour = Math.floor(Math.random()*16777215).toString(16);
-                e.target.style.backgroundColor = "#" + randomColour;
-                e.target.style.borderColor = "#" + randomColour;
-            }
-            else {
-                e.target.classList.add(`ink${colour}`);
-            }
-        }
-    }   
-  });
-
-canvas.addEventListener('mouseup', function (e) {
-    if (isDrawing) {
-        isDrawing = false;
-    }   
-  });
+function drawOver(target) {
+    let currentColour = target.classList[1];
+    target.classList.remove(currentColour);
+    target.style.removeProperty("background-color");
+    target.style.removeProperty("border-color");
+    return;
+}
 
 function getColour() {
     colourChoice = document.querySelector('input[name="colourChoice"]:checked').value;
-    if (colourChoice == "red") { 
-        return "Red";
-    }
-    else if (colourChoice == "orange") {
-        return "Orange";
-    }
-    else if (colourChoice == "yellow") {
-        return "Yellow";
-    }
-    else if (colourChoice == "green") {
-        return "Green";
-    }
-    else if (colourChoice == "blue") {
-        return "Blue";
-    }
-    else if (colourChoice == "indigo") {
-        return "Indigo";
-    }
-    else if (colourChoice == "violet") {
-        return "Violet";
-    }
-    else if (colourChoice == "pink") {
-        return "Pink";
-    }
-    else if (colourChoice == "brown") {
-        return "Brown";
-    }
-    else if (colourChoice == "black") {
-        return "Black";
-    }
-    else if (colourChoice == "white") {
-        return "White";
-    }
-    else if (colourChoice == "grey") {
-        return "Grey";
-    }
-    else if (colourChoice == "eraser") {
-        return "None";
-    }
-    else if (colourChoice == "random") {
-        return "Random";
-    }
+    return colourChoice;
   };
